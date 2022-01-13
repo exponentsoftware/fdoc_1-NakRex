e545f091c66eb9492b96ba0f2b8eec66ae607172
NakRex <khannasimahmad@gmail.com>
2022-01-13 19:25:29 +0530

    fixed the function

--- countWords.js
+++ countWords.js
@@ -1,11 +1,9 @@
 function countWords(para, word1, word2) {
-    let paraArr = para.split(" ");
+    let paraArr = para.split(" "||".");
     let count1 = 0; let count2=0;
     paraArr.forEach(element => {
-        
         if (element == word1)
             count1++;
-        
         if (element === word2)
             count2++;
     });
@@ -17,6 +15,6 @@ function countWords(para, word1, word2) {
 
 
 
-let paragraph = 'I love teaching. If you do not love teaching what else can you love . I love JavaScript if you do not love something which can give life to your application what else can you love';
+let paragraph = 'I love teaching. If you do not love teaching what else can you love. I love JavaScript if you do not love something which can give life to your application what else can you love.';
 
 console.log(countWords(paragraph, "love", "you")); 
