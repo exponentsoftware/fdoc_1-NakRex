d223413cc557750e274770e1521872608928d220
NakRex <khannasimahmad@gmail.com>
2022-01-14 20:27:05 +0530

    fixed uniqueRandom function

--- uniqueRandom.js
+++ uniqueRandom.js
@@ -2,13 +2,13 @@
 // All the numbers must be unique.
 
 function randomUnique() {
-    array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
         for (let i = array.length - 1; i > 0; i--) {
             let j = Math.floor(Math.random() * (i + 1));
             let temp = array[i];
             array[i] = array[j];
             array[j] = temp;
         }
-   return array.slice(2)
+   return array.slice(3)
 }
 console.log(randomUnique());
